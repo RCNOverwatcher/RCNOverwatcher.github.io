4287a0d0fc502733ff47a9dde4d37d963bdbb0ab
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2022-10-18 21:34:11 +0100

    update for today

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -11,7 +11,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '264637932922143477181112478829382995901'                       // used to check whether the localStorage data is stale
+var gzip_hash = '180162952157691700057847321091920171472'                       // used to check whether the localStorage data is stale
 
 
 // Onloads
