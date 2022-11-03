c5dd1891d2ea5aa5c628fb938624447964d9325b
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2022-11-03 12:58:03 +0000

    fm and maths 03/11/22

--- notes/obs.html/static/obsidian_core.js
+++ notes/obs.html/static/obsidian_core.js
@@ -11,7 +11,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/notes";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '87247110212723370341203535005579772272'                       // used to check whether the localStorage data is stale
+var gzip_hash = '61634427254811977073440482750841737151'                       // used to check whether the localStorage data is stale
 
 
 // Onloads
