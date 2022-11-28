f9457e7b4e57761878ee822876243bcbfeee59f4
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2022-11-28 13:03:37 +0000

    quick fix

--- notes/obs.html/static/obsidian_core.js
+++ notes/obs.html/static/obsidian_core.js
@@ -11,7 +11,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/notes";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '314894184073680889709207307031675574728'                       // used to check whether the localStorage data is stale
+var gzip_hash = '115703393110358832798629112115468471073'                       // used to check whether the localStorage data is stale
 
 
 // Onloads
